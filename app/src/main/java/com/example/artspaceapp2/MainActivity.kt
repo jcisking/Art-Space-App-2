b7ceb9d583704c958845b7b29c2cf3e003ae6c0e
jcisking <krisloeffler@gmail.com>
2024-02-12 21:37:44 -0700

    modified Artwork

--- app/src/main/java/com/example/artspaceapp2/MainActivity.kt
+++ app/src/main/java/com/example/artspaceapp2/MainActivity.kt
@@ -3,19 +3,29 @@ package com.example.artspaceapp2
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardElevation
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.ShapeDefaults
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
@@ -43,14 +53,27 @@ class MainActivity : ComponentActivity() {
 fun Artwork(
     modifier: Modifier = Modifier
 ) {
-    Column(modifier = modifier) {
-        Box() {
+    Column(
+        modifier = modifier,
+
+    ) {
+
+        Surface(modifier = Modifier
+            .padding(horizontal = 15.dp),
+            shadowElevation = 15.dp,
+            color = Color.White,
+//            border = BorderStroke(6.dp, Color.LightGray)
+        ) {
             Image(
-                painter = painterResource(id = R.drawable.placeholder
+                modifier = Modifier
+                    .padding(20.dp),
+                painter = painterResource(
+                    id = R.drawable.placeholder
                 ),
                 contentDescription = null
             )
         }
+
         Box() {
             Column {
                 Text(text = "sdfsf")
