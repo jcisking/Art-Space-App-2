354ad61478236818cdce70261def62f87566e686
jcisking <gocats098@gmail.com>
2024-02-16 12:45:03 -0700

    added photos and data class

--- app/src/main/java/com/example/artspaceapp2/MainActivity.kt
+++ app/src/main/java/com/example/artspaceapp2/MainActivity.kt
@@ -3,16 +3,16 @@ package com.example.artspaceapp2
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
@@ -21,7 +21,6 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.modifier.modifierLocalConsumer
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -85,7 +84,7 @@ fun ArtworkImage(
             modifier = Modifier
                 .padding(32.dp),
             painter = painterResource(
-                id = R.drawable.placeholder
+                id = R.drawable.darren_and_lovee
             ),
             contentDescription = null
         )
@@ -135,6 +134,14 @@ fun ButtonRow(
             Text(text = "Next")
         }
     }
+}
+
+data class Artwork(
+    @DrawableRes val imageRes: Int,
+    @StringRes val description: Int,
+    @StringRes val artist: Int,
+) {
+
 }
 
 
